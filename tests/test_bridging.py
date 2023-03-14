8851037aaf4249f187c802f958e3a92fee281e7d
Denes Fekete <denes710@gmail.com>
2023-03-14 10:29:49 +0100

    test: cases added for bid challenges

--- tests/test_bridging.py
+++ tests/test_bridging.py
@@ -26,7 +26,7 @@ def init_contracts():
 
     return srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721
 
-def test_one_token_briging_circle(init_contracts):
+def test_one_token_briging_circle_without_challenge(init_contracts):
     srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
 
     user = accounts[1]
@@ -73,6 +73,72 @@ def test_challenge_on_source_during_locking(init_contracts):
     retRelayer = srcSpokeBridge.relayers(relayer)
     assert retRelayer["status"] == 4
 
+def test_false_challenge_on_source_during_locking(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+    dstSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    # before time window sending the proof of # id incoming message
+    with reverts("SrcSpokeBridge: Time window is not expired!"):
+        dstSpokeBridge.sendProof(False, 0, {'from': challenger})
+
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    # relaying
+    dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
+
+    with reverts("DstSpokeBridge: too early to send proof!"):
+        dstSpokeBridge.sendProof(False, 0, {'from': challenger})
+
+    # it's 4 hours
+    chain.sleep(14400000)
+    # after time window sending the proof of # id incoming message
+    with reverts("SrcSpokeBridge: False challenging!"):
+        dstSpokeBridge.sendProof(False, 0, {'from': challenger})
+
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+def test_false_challenge_on_source_during_locking_wrong_proof(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    # sending the proof of # id outgoing message
+    with reverts("SrcSpokeBrdige: There is no corresponding local bid!"):
+        dstSpokeBridge.sendProof(True, 0, {'from': challenger})
+
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    # no relaying
+    chain.sleep(14400000) # it's 4 hours
+
+    # sending the proof of # id outoging message
+    with reverts("SrcSpokeBrdige: There is no corresponding local bid!"):
+        dstSpokeBridge.sendProof(True, 0, {'from': challenger})
+
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
 def test_challenge_on_dest_during_burning(init_contracts):
     srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
 
@@ -89,21 +155,106 @@ def test_challenge_on_dest_during_burning(init_contracts):
 
     dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
 
-    chain.sleep(14400000) # it's 4 hours
+    # it's 4 hours
+    chain.sleep(14400000)
 
     wrappedErc721.approve(dstSpokeBridge.address, 1, {'from': receiver})
 
     dstSpokeBridge.createBid(user, 1, wrappedErc721.address, 0, {'from': receiver, 'amount': Wei("0.01 ether")})
     dstSpokeBridge.buyBid(0, {'from': relayer})
 
-    # FIXME maybe it is neeeded chain.sleep(14400000) # it's 4 hours
-
+    # it's 4 hours
+    chain.sleep(14400000)
     # sending the proof of # id incoming message
     srcSpokeBridge.sendProof(False, 0, {'from': challenger})
 
     # FIXME more check - relayer - challenger money
     retRelayer = dstSpokeBridge.relayers(relayer)
     assert retRelayer["status"] == 4
+
+def test_false_challenge_on_dest_during_burning(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+    dstSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
+
+    # it's 4 hours
+    chain.sleep(14400000)
+
+    wrappedErc721.approve(dstSpokeBridge.address, 1, {'from': receiver})
+
+    dstSpokeBridge.createBid(user, 1, wrappedErc721.address, 0, {'from': receiver, 'amount': Wei("0.01 ether")})
+    dstSpokeBridge.buyBid(0, {'from': relayer})
+
+    # before time window sending the proof of # id incoming message
+    with reverts("DstSpokeBridge: Time window is not expired!"):
+        srcSpokeBridge.sendProof(False, 0, {'from': challenger})
+
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    # relaying
+    srcSpokeBridge.unlocking(0, 0, user, 1, wrappedErc721.address, {'from': relayer});
+
+    with reverts("SrcSpokeBridge: too early to send proof!"):
+        srcSpokeBridge.sendProof(False, 0, {'from': challenger})
+
+    # it's 4 hours
+    chain.sleep(14400000)
+    # after time window sending the proof of # id incoming message
+    with reverts("DstSpokeBridge: False challenging!"):
+        srcSpokeBridge.sendProof(False, 0, {'from': challenger})
+
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+def test_false_challenge_on_dest_during_burning_wrong_proof(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+    dstSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
+
+    # it's 4 hours
+    chain.sleep(14400000)
+
+    wrappedErc721.approve(dstSpokeBridge.address, 1, {'from': receiver})
+
+    dstSpokeBridge.createBid(user, 1, wrappedErc721.address, 0, {'from': receiver, 'amount': Wei("0.01 ether")})
+    dstSpokeBridge.buyBid(0, {'from': relayer})
+
+    # sending the proof of # id incoming message
+    with reverts("DstSpokeBridge: Time window is expired!"):
+        srcSpokeBridge.sendProof(True, 0, {'from': challenger})
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    # it's 4 hours
+    chain.sleep(14400000)
+    # sending the proof of # id incoming message
+    with reverts("DstSpokeBridge: Time window is expired!"):
+        srcSpokeBridge.sendProof(True, 0, {'from': challenger})
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
 
 def test_challenge_on_source_during_unlocking(init_contracts):
     srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
@@ -126,11 +277,98 @@ def test_challenge_on_source_during_unlocking(init_contracts):
 
     # challenging
     srcSpokeBridge.challengeUnlocking(0, {'from': challenger, 'amount': Wei("10 ether")});
-    # FIXME it is problematic, because it is working with "False as well"
     dstSpokeBridge.sendProof(True, 0, {'from': challenger})
 
     retRelayer = srcSpokeBridge.relayers(relayer)
     assert retRelayer["status"] == 4
+
+def test_false_challenge_on_source_during_unlocking(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+    dstSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
+
+    chain.sleep(14400000) # it's 4 hours
+
+    wrappedErc721.approve(dstSpokeBridge.address, 1, {'from': receiver})
+
+    dstSpokeBridge.createBid(user, 1, wrappedErc721.address, 0, {'from': receiver, 'amount': Wei("0.01 ether")})
+    dstSpokeBridge.buyBid(0, {'from': relayer})
+
+    # false challenging before relaying
+    with reverts("SrcSpokeBridge: Corresponding incoming bid status is not relayed!"):
+        srcSpokeBridge.challengeUnlocking(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("SrcSpokeBrdige: There is no corresponding local bid!"):
+        dstSpokeBridge.sendProof(True, 0, {'from': challenger})
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    # relaying
+    srcSpokeBridge.unlocking(0, 0, user, 1, wrappedErc721.address, {'from': relayer});
+
+    chain.sleep(14400000) # it's 4 hours
+
+    # challenging after time window
+    with reverts("SrcSpokeBridge: The dispute period is expired!"):
+        srcSpokeBridge.challengeUnlocking(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("SrcSpokeBridge: Time window is expired!"):
+        dstSpokeBridge.sendProof(True, 0, {'from': challenger})
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+def test_false_challenge_on_source_during_unlocking_wrong_proof(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+    dstSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
+
+    chain.sleep(14400000) # it's 4 hours
+
+    wrappedErc721.approve(dstSpokeBridge.address, 1, {'from': receiver})
+
+    dstSpokeBridge.createBid(user, 1, wrappedErc721.address, 0, {'from': receiver, 'amount': Wei("0.01 ether")})
+    dstSpokeBridge.buyBid(0, {'from': relayer})
+
+    # false challenging before relaying
+    with reverts("SrcSpokeBridge: Corresponding incoming bid status is not relayed!"):
+        srcSpokeBridge.challengeUnlocking(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("SrcSpokeBridge: False challenging!"):
+        dstSpokeBridge.sendProof(False, 0, {'from': challenger})
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    # relaying
+    srcSpokeBridge.unlocking(0, 0, user, 1, wrappedErc721.address, {'from': relayer});
+
+    chain.sleep(14400000) # it's 4 hours
+
+    # challenging after time window
+    with reverts("SrcSpokeBridge: The dispute period is expired!"):
+        srcSpokeBridge.challengeUnlocking(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("SrcSpokeBridge: False challenging!"):
+        dstSpokeBridge.sendProof(False, 0, {'from': challenger})
+    retRelayer = srcSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
 
 def test_challenge_on_dest_during_minting(init_contracts):
     srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
@@ -151,3 +389,73 @@ def test_challenge_on_dest_during_minting(init_contracts):
 
     retRelayer = dstSpokeBridge.relayers(relayer)
     assert retRelayer["status"] == 4
+
+def test_false_challenge_on_dest_during_minting(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+    dstSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    # challenging
+    with reverts("DstSpokeBridge: Corresponding incoming bid status is not relayed!"):
+        dstSpokeBridge.challengeMinting(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("DstSpokeBrdige: There is no corresponding local bid!"):
+        srcSpokeBridge.sendProof(True, 0, {'from': challenger})
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
+
+    # challenge period
+    chain.sleep(14400000) # it's 4 hours
+
+    # challenging
+    with reverts("DstSpokeBridge: The dispute period is expired!"):
+        dstSpokeBridge.challengeMinting(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("DstSpokeBridge: Time window is expired!"):
+        srcSpokeBridge.sendProof(True, 0, {'from': challenger})
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+def test_challenge_on_dest_during_minting_wrong_proof(init_contracts):
+    srcSpokeBridge, dstSpokeBridge, contractMap, erc721, wrappedErc721 = init_contracts
+
+    user = accounts[1]
+    challenger = accounts[2]
+    receiver = accounts[3]
+    relayer = accounts[4]
+
+    srcSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+    dstSpokeBridge.deposite({'from': relayer, 'amount': Wei("20 ether")})
+
+    srcSpokeBridge.createBid(receiver, 1, erc721.address, {'from': user, 'amount': Wei("0.01 ether")})
+    srcSpokeBridge.buyBid(0, {'from': relayer})
+
+    # challenging
+    with reverts("DstSpokeBridge: Corresponding incoming bid status is not relayed!"):
+        dstSpokeBridge.challengeMinting(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("DstSpokeBrdige: There is no corresponding local bid!"):
+        srcSpokeBridge.sendProof(False, 0, {'from': challenger})
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
+
+    dstSpokeBridge.minting(0, receiver, 1, wrappedErc721.address, {'from': relayer})
+
+    # challenge period
+    chain.sleep(14400000) # it's 4 hours
+
+    # challenging
+    with reverts("DstSpokeBridge: The dispute period is expired!"):
+        dstSpokeBridge.challengeMinting(0, {'from': challenger, 'amount': Wei("10 ether")});
+    with reverts("DstSpokeBrdige: There is no corresponding local bid!"):
+        srcSpokeBridge.sendProof(False, 0, {'from': challenger})
+    retRelayer = dstSpokeBridge.relayers(relayer)
+    assert retRelayer["status"] == 1
